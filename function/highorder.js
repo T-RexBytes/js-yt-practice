c3069989dc68c2a887742b4f16f96aa28328c8aa
TRexBytes <azurebliss12@gmail.com>
2025-09-06 11:10:55 +0000

    for of loop

---
+++ function/highorder.js
@@ -0,0 +1,11 @@
+// const arr = [1, 2, 3, 4, 5];
+// arr.forEach(val => {
+//     console.log(val);
+// });
+
+// const string = "Sourish Panda";
+// for(const char of string){
+//     if(char === ' ') continue;
+//     console.log(`the letter is ${char}`);
+// }
+
