d215623388e2ccac5419e5d3f041275429dfd993
TRexBytes <azurebliss12@gmail.com>
2025-09-06 05:34:55 +0000

    learnt about this and arrowfuntions

---
+++ funtion/this&arrowfunc.js
@@ -0,0 +1,22 @@
+const user={
+    username : "Sourish",
+    age : 18,
+    welcomefuntion : function() {
+        console.log(`${this.username}, Welcome to the website`)
+        // console.log(this);
+    }
+}
+// user.welcomefuntion();
+// user.username = "Alex";
+// user.welcomefuntion();
+// console.log(this);
+// const addtwo = (num1, num2) => {
+//     return num1+num2;
+// }
+// const addtwo = (num1, num2) => (num1+num2);
+// const addtwo = (num1, num2) => num1+num2;
+// console.log(addtwo(3, 4));
+
+
+// IIFE 
+// Immidiately Invoked Function Expressions
